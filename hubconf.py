65fc81fa8f1079baccc52bec55b388e28c2a7c85
Vivek Mani Charan P <cs19b032@iittp.ac.in>
2022-11-29 10:01:20 +0530

    tmp

---
+++ hubconf.py
@@ -0,0 +1,21 @@
+from PIL import Image
+def resize(image):
+  im = Image.open(image)
+  print(f"Original size : {im.size}")
+  im = im.resize((400, 400))
+  return im
+
+
+
+# def resize(image):
+#   image = request.files['img']
+#   # image_path = "./images/" + image.filename
+#   # image.save(image_path)
+#   image = Image.open(image.stream)
+#   print(f"Original size : {image.size}") # 5464x3640
+#   sunset_resized = image.resize((400, 400))
+#   sunset_resized.save('resized.jpeg')
+#   try:
+#     return send_file('resized.jpeg', download_name='resized.png', mimetype='image/png')
+#   except Exception as e:
+#     return str(e)
